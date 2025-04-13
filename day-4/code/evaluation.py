ac6d9024e5d037dd4d4f01ab1983f024669aad6c
naveen <naveenkumarspa@gmail.com>
2025-04-14 04:13:58 +0530

    evaluation updated to gemini

--- day-4/code/evaluation.py
+++ day-4/code/evaluation.py
@@ -3,11 +3,15 @@ import os
 from tqdm.notebook import tqdm
 from datasets import load_dataset
 from qdrant_client import QdrantClient
-from langchain_google_genai import ChatGoogleGenerativeAI
 from tqdm import tqdm
 from langchain.docstore.document import Document as LangchainDocument
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 import deepeval
+from pydantic import BaseModel
+from google import genai
+from google.genai import types
+import instructor
+from deepeval.models import DeepEvalBaseLLM
 
 
 # Get a FREE forever cluster at https://cloud.qdrant.io/
@@ -29,6 +33,31 @@ if not GOOGLE_API_KEY:
 EVAL_SIZE = 10
 RETRIEVAL_SIZE = 3
 
+
+class CustomGeminiFlash(DeepEvalBaseLLM):
+    def __init__(self):
+        self.model = genai.Client(api_key=GOOGLE_API_KEY)
+
+    def load_model(self):
+        return self.model
+
+    def generate(self, prompt: str) -> BaseModel:
+        client = self.load_model()
+        response = client.models.generate_content(
+            model="gemini-2.0-flash",
+            contents=prompt,
+            config=types.GenerateContentConfig(
+                temperature=0
+            ),
+        )
+        return response.text
+
+    async def a_generate(self, prompt: str) -> BaseModel:
+        return self.generate(prompt)
+
+    def get_model_name(self):
+        return "Gemini 2.0 Flash"
+
 dataset = load_dataset("atitaarora/qdrant_doc", split="train")
 
 langchain_docs = [
@@ -49,7 +78,7 @@ docs_processed = []
 for doc in langchain_docs:
     docs_processed += text_splitter.split_documents([doc])
 
-client = QdrantClient(url=QDRANT_URL, api_key=QDRANT_API_KEY)
+qdrant_client = QdrantClient(url=QDRANT_URL, api_key=QDRANT_API_KEY)
 
 docs_contents, docs_metadatas = [], []
 
@@ -65,24 +94,16 @@ for doc in docs_processed:
 # Uses FastEmbed - https://qdrant.tech/documentation/fastembed/
 # To generate embeddings for the documents
 # The default model is `BAAI/bge-small-en-v1.5`
-client.add(
+qdrant_client.add(
     collection_name=COLLECTION_NAME,
     metadata=docs_metadatas,
     documents=docs_contents,
 )
 
-llm = ChatGoogleGenerativeAI(
-    model="gemini-1.5-flash",  # Use the model you prefer
-    google_api_key=GOOGLE_API_KEY,
-    temperature=0.2,  # Lower temperature for more consistent classification
-)
-
-# openai_client = OpenAI(api_key=OPENAI_API_KEY)
-
 
 def query_with_context(query, limit):
 
-    search_result = client.query(
+    search_result = qdrant_client.query(
         collection_name=COLLECTION_NAME, query_text=query, limit=limit
     )
 
@@ -110,22 +131,16 @@ def query_with_context(query, limit):
 
     prompt = prompt_start + "\n\n---\n\n".join(contexts) + prompt_end
 
-    res = llm.invoke(prompt)
-
+    gemini_client = genai.Client(api_key=GOOGLE_API_KEY)
+    res = gemini_client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=prompt,
+        config=types.GenerateContentConfig(
+            temperature=0
+        ),
+    )
     print("response", res)
-
-    # res = openai_client.completions.create(
-    #     model="gpt-3.5-turbo-instruct",
-    #     prompt=prompt,
-    #     temperature=0,
-    #     max_tokens=636,
-    #     top_p=1,
-    #     frequency_penalty=0,
-    #     presence_penalty=0,
-    #     stop=None,
-    # )
-
-    return (contexts, res.choices[0].text)
+    return (contexts, res.text)
 
 
 qdrant_qna_dataset = load_dataset("atitaarora/qdrant_doc_qna", split="train")
@@ -154,13 +169,14 @@ test_cases = create_deepeval_dataset(
     qdrant_qna_dataset, EVAL_SIZE, RETRIEVAL_SIZE
 )
 
+evaluation_model=CustomGeminiFlash()
 deepeval.evaluate(
     test_cases=test_cases,
     metrics=[
-        deepeval.metrics.AnswerRelevancyMetric(),
-        deepeval.metrics.FaithfulnessMetric(),
-        deepeval.metrics.ContextualPrecisionMetric(),
-        deepeval.metrics.ContextualRecallMetric(),
-        deepeval.metrics.ContextualRelevancyMetric(),
+        deepeval.metrics.AnswerRelevancyMetric(model=evaluation_model),
+        deepeval.metrics.FaithfulnessMetric(model=evaluation_model),
+        deepeval.metrics.ContextualPrecisionMetric(model=evaluation_model),
+        deepeval.metrics.ContextualRecallMetric(model=evaluation_model),
+        deepeval.metrics.ContextualRelevancyMetric(model=evaluation_model),
     ],
 )
