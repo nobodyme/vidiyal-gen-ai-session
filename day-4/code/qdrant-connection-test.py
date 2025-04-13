dbf695c9b87973a32b10a470338f368428315d02
naveen <naveenkumarspa@gmail.com>
2025-04-13 22:44:45 +0530

    used env vars in qdrant test

--- day-4/code/qdrant-connection-test.py
+++ day-4/code/qdrant-connection-test.py
@@ -1,8 +1,17 @@
+import os
 from qdrant_client import QdrantClient
 
+
+QDRANT_URL = os.environ.get("QDRANT_CLUSTER_URL")  
+QDRANT_API_KEY = os.environ.get("QDRANT_API_KEY")
+if not QDRANT_URL or not QDRANT_API_KEY:
+    print("Error: QDRANT_CLUSTER_URL, QDRANT_API_KEY not found in environment variables."
+          "Please set them in your .env file or directly in the environment.")
+    exit()
+
 qdrant_client = QdrantClient(
-    url="YOUR_CLUSTER_URL", 
-    api_key="YOUR_QDRANT_API_KEY",
+    url=QDRANT_URL, 
+    api_key=QDRANT_API_KEY,
 )
 
 print(qdrant_client.get_collections())
